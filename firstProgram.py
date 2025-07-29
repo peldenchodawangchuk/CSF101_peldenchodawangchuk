dd2770bfcd39fad941598675529f450340b4d4f5
pelden choda wangchuk <gokupelden@gmail.com>
2025-07-29 14:37:10 +0600

    life of a 1swe student

--- firstProgram.py
+++ firstProgram.py
@@ -1,2 +1,9 @@
-print('hello SWE')
-print("help me")
+print('hello SWE')#
+print("help me")#
+a=4
+b=4
+sum=a+b
+print("the sum of a and b is:",sum)
+name = input("enter name")
+age = input("enter ur age")
+print("youre name is",name,"and you are",age,"years old")
